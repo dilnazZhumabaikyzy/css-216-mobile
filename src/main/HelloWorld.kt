8e12dc13a5388bd1596f01ab6e6b963abdd24701
Dilnaz Zhumabaikyzy <dilnazzh200303@gmail.com>
2022-09-14 00:43:31 +0600

    2.4.3

--- src/main/HelloWorld.kt
+++ src/main/HelloWorld.kt
@@ -25,12 +25,18 @@ fun feedTheFish() {
 }
 fun shouldChangeWater (day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
     return when {
-        temperature > 30 -> true
-        dirty > 30 -> true
-        day == "Sunday" ->  true
-        else -> false
+        isTooHot(temperature) -> true
+        isDirty(dirty) -> true
+        isSunday(day) -> true
+        else  -> false
     }
 }
+//compact functions
+fun isTooHot(temperature: Int) = temperature > 30
+
+fun isDirty(dirty: Int) = dirty > 30
+
+fun isSunday(day: String) = day == "Sunday"
 
 fun main(args: Array<String>) {
     feedTheFish()
