59a37566ace30b22304d078a0bfb110f885da2f0
Dilnaz Zhumabaikyzy <dilnazzh200303@gmail.com>
2022-09-14 00:41:43 +0600

    2.4.2

--- src/main/HelloWorld.kt
+++ src/main/HelloWorld.kt
@@ -1,10 +1,37 @@
 import java.util.*    // required import
 
-fun swim(speed: String = "fast") {
-    println("swimming $speed")
+fun randomDay() : String {
+    val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
+        "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(week.size)]
 }
+
+fun fishFood (day : String) : String {
+    return when (day) {
+        "Monday" -> "flakes"
+        "Wednesday" -> "redworms"
+        "Thursday" -> "granules"
+        "Friday" -> "mosquitoes"
+        "Sunday" -> "plankton"
+        else -> "nothing"
+    }
+}
+
+fun feedTheFish() {
+    val day = randomDay()
+    val food = fishFood(day)
+    println ("Today is $day and the fish eat $food")
+    println("Change water: ${shouldChangeWater(day)}")
+}
+fun shouldChangeWater (day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
+    return when {
+        temperature > 30 -> true
+        dirty > 30 -> true
+        day == "Sunday" ->  true
+        else -> false
+    }
+}
+
 fun main(args: Array<String>) {
-    swim()   // uses default speed
-    swim("slow")   // positional argument
-    swim(speed="turtle-like")   // named parameter
+    feedTheFish()
 }
