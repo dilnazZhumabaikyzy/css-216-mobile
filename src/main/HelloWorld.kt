7fa39ccc3980d98ee4fdc55ef02a159a674fd490
Dilnaz Zhumabaikyzy <dilnazzh200303@gmail.com>
2022-09-14 00:30:48 +0600

    2.2.4

--- src/main/HelloWorld.kt
+++ src/main/HelloWorld.kt
@@ -1,6 +1,6 @@
 // Will assign kotlin.Unit
 fun main(args: Array<String>) {
-
-    val isUnit = println("This is an expression")
-    println(isUnit)
+    val temperature = 200
+    val isHot = if (temperature > 150) true else false
+    println(isHot)
 }
