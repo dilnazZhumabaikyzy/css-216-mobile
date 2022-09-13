2a934ce3365775b72fa088d17aa5b9e48dcc2749
Dilnaz Zhumabaikyzy <dilnazzh200303@gmail.com>
2022-09-14 00:48:03 +0600

    2.5.2

--- src/main/HelloWorld.kt
+++ src/main/HelloWorld.kt
@@ -1,5 +1,9 @@
-val decorations = listOf ("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
-
 fun main() {
-    println( decorations.filter {it[0] == 'p'})
+    val decorations = listOf ("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
+
+    // lazy
+    val filtered = decorations.asSequence().filter { it[0] == 'p' }
+    println("filtered: $filtered")
+
+    // output filtered: kotlin.sequences.FilteringSequence@37f8bb67
 }
