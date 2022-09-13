30e05828f5c16fac103278c20f7aa59f63f29de0
Dilnaz Zhumabaikyzy <dilnazzh200303@gmail.com>
2022-09-14 00:45:12 +0600

    2.5.1

--- src/main/HelloWorld.kt
+++ src/main/HelloWorld.kt
@@ -1,43 +1,5 @@
-import java.util.*    // required import
+val decorations = listOf ("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
 
-fun randomDay() : String {
-    val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
-        "Friday", "Saturday", "Sunday")
-    return week[Random().nextInt(week.size)]
-}
-
-fun fishFood (day : String) : String {
-    return when (day) {
-        "Monday" -> "flakes"
-        "Wednesday" -> "redworms"
-        "Thursday" -> "granules"
-        "Friday" -> "mosquitoes"
-        "Sunday" -> "plankton"
-        else -> "nothing"
-    }
-}
-
-fun feedTheFish() {
-    val day = randomDay()
-    val food = fishFood(day)
-    println ("Today is $day and the fish eat $food")
-    println("Change water: ${shouldChangeWater(day)}")
-}
-fun shouldChangeWater (day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
-    return when {
-        isTooHot(temperature) -> true
-        isDirty(dirty) -> true
-        isSunday(day) -> true
-        else  -> false
-    }
-}
-//compact functions
-fun isTooHot(temperature: Int) = temperature > 30
-
-fun isDirty(dirty: Int) = dirty > 30
-
-fun isSunday(day: String) = day == "Sunday"
-
-fun main(args: Array<String>) {
-    feedTheFish()
+fun main() {
+    println( decorations.filter {it[0] == 'p'})
 }
