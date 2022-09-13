01f376f9f5826f5d458e0b608262dbfdef4fe37a
Dilnaz Zhumabaikyzy <dilnazzh200303@gmail.com>
2022-09-14 00:59:27 +0600

    2.6.1.2

--- src/main/HelloWorld.kt
+++ src/main/HelloWorld.kt
@@ -1,5 +1,9 @@
 fun main() {
-    var dirtyLevel = 20
-    val waterFilter = { dirty : Int -> dirty / 2}
-    println(waterFilter(dirtyLevel))
+    val waterFilter: (Int) -> Int = { dirty -> dirty / 2 }
+    println(waterFilter(14))
+
+    //Make a variable called waterFilter.
+    //waterFilter can be any function that takes an Int and returns an Int.
+    //Assign a lambda to waterFilter.
+    //The lambda returns the value of the argument dirty divided by 2.
 }
