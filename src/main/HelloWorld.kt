a99f8d4a812995a0f6dbb6153609227422586d1c
Dilnaz Zhumabaikyzy <dilnazzh200303@gmail.com>
2022-09-14 01:05:28 +0600

    2.6.2.1-2

--- src/main/HelloWorld.kt
+++ src/main/HelloWorld.kt
@@ -1,9 +1,15 @@
 fun main() {
-    val waterFilter: (Int) -> Int = { dirty -> dirty / 2 }
-    println(waterFilter(14))
+    //a higher-order function
+    //A higher-order function is a function that takes other functions as parameters,
+    // or it is a function that returns another function.
 
-    //Make a variable called waterFilter.
-    //waterFilter can be any function that takes an Int and returns an Int.
-    //Assign a lambda to waterFilter.
-    //The lambda returns the value of the argument dirty divided by 2.
+
+    fun updateDirty(dirty: Int, operation: (Int) -> Int): Int {
+        return operation(dirty)
+    }
+    // The first argument is an integer.
+    // The second argument is a function that takes an integer and returns an integer.
+
+    val waterFilter: (Int) -> Int = { dirty -> dirty / 2 }
+    println(updateDirty(30, waterFilter))
 }
