0744b18317e656243f2ca16799638f932ac5a5c9
Dilnaz Zhumabaikyzy <dilnazzh200303@gmail.com>
2022-09-14 13:17:04 +0600

    3.8.1-2 data class

--- src/main/myapp/Main.kt
+++ src/main/myapp/Main.kt
@@ -3,10 +3,9 @@ import java.lang.Math
 fun makeFish() {
     val shark = Shark()
     val pleco = Plecostomus()
+
     println("Shark: ${shark.color}")
-    shark.eat()
     println("Plecostomus: ${pleco.color}")
-    pleco.eat()
 }
 
 fun main () {
