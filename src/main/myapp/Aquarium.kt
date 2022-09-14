4b03d9cf46928b7f13d1159846408958179e0de5
Dilnaz Zhumabaikyzy <dilnazzh200303@gmail.com>
2022-09-14 08:42:03 +0600

    3.3.2.1

--- src/main/myapp/Aquarium.kt
+++ src/main/myapp/Aquarium.kt
@@ -1,6 +1,15 @@
 package main.myapp
 class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
-    // Dimensions in cm
+    //The example constructors above just declare properties and assign the value of an expression to them.
+    //If your constructor needs more initialization code, it can be placed in one or more init blocks.
+
+    init {
+        println("aquarium initializing")
+    }
+    init {
+        // 1 liter = 1000 cm^3
+        println("Volume: ${width * length * height / 1000} liters")
+    }
 
     fun printSize() {
         println("Width: $width cm " +
