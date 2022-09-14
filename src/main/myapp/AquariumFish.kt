4cb6d6df6839cf7e3d315c34085bb6d693523b2b
Dilnaz Zhumabaikyzy <dilnazzh200303@gmail.com>
2022-09-14 13:06:30 +0600

    3.7.2-4

--- src/main/myapp/AquariumFish.kt
+++ src/main/myapp/AquariumFish.kt
@@ -1,7 +1,13 @@
 package main.myapp
 
+//Kotlin lets you declare a class where you can only create one instance of it by using the keyword object instead of class.
+//Kotlin will create that one instance, and that instance is referenced by the class name.
+//Then all other objects can just use this one instance. You cannot create other instances of this class.
 interface FishColor {
     val color: String
+}
+object GoldColor : FishColor {
+    override val color = "gold"
 }
 
 class Shark: FishAction, FishColor{
@@ -10,11 +16,12 @@ class Shark: FishAction, FishColor{
         println("hunt and eat fish")
     }
 }
-
-class Plecostomus: FishAction, FishColor {
-    override val color = "gold"
+class Plecostomus (fishColor: FishColor = GoldColor):
+    FishAction by PrintingFishAction("eat algae"),
+    FishColor by fishColor
+class PrintingFishAction(val food: String) : FishAction {
     override fun eat() {
-        println("eat algae")
+        println(food)
     }
 }
 interface FishAction  {
